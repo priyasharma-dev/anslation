9986339d499becadf18e7895cd04d455987342db
samraat <nitish.kumar@anslation.com>
2025-09-30 10:58:04 +0530

    About: update AboutSection.jsx

--- src/components/Aboutus/AboutSection.jsx
+++ src/components/Aboutus/AboutSection.jsx
@@ -7,8 +7,8 @@ function AboutSection() {
             <div className='w-[90%] my-32 mx-auto'>
                 <div className='flex justify-between items-center'>
                     <div className=' w-xl' >
-                        <p>About us</p>
-                        <h2 className='text-6xl' >Empowering with</h2>
+                        <p className='text-xl'> About us</p>
+                        <h2 className='text-6xl' >Empowering with </h2>
                         <h1 className='text-6xl border-0 font-extrabold bg-[#1454E5] rounded px-1 ' >Innovative Software</h1>
                         <div className='my-2.5'>
                             <span className='font-extrabold text-6xl border-0 bg-[#1454E5] rounded px-2' >Solutions</span>
